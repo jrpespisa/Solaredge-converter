7270c70160d232c2144058ae0b2dfa6072d7bd45
jrpespisa <jrpespisa@verizon.net>
2016-02-25 14:40:16 -0500

    successfully recieved data from site inverter

--- app/models/query/solar_data.rb
+++ app/models/query/solar_data.rb
@@ -1,10 +1,9 @@
 class SolarData
   def initialize
     @key = ENV["API_KEY"]
-    @uri = URI("https://monitoringapi.solaredge.com/site/171384/energy?startDate=2016-02-2&endDate=2016-07-2?api_key=#{@key}")
-    # @uri = URI("https://monitoringapi.solaredge.com/equipment/171384/sensors?api_key=#{@key}")
+    @uri = URI("https://monitoringapi.solaredge.com/site/171384/energy?timeUnit=DAY&endDate=2016-02-07&startDate=2016-02-01&api_key=#{@key}")
     @response = Net::HTTP.get_response(@uri)
-    # @info = JSON.parse(@response.body)
+    @info = JSON.parse(@response.body)
     binding.pry
   end
 end
